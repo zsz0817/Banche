81f239e14b9b30dd986b92a743a6c2bebd938642
zsz0817 <353586930@qq.com>
2018-01-26 13:49:32 +0800

    导航activity

---
+++ app/src/main/java/com/example/shizhuan/banche/Search/NaviActivity.java
@@ -0,0 +1,18 @@
+package com.example.shizhuan.banche.Search;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by ShiZhuan on 2018/1/26.
+ */
+
+public class NaviActivity extends Activity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
